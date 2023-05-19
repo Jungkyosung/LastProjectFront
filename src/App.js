35a54cab96d10ce0a51778b5bbd9ce05c06694bc
lsy00812 <lsy00812@gmail.com>
2023-05-19 14:40:42 +0900

    0519

--- src/App.js
+++ src/App.js
@@ -1,65 +1,67 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Regist from './regist/Regist';
-import NotFound from './not_found/NotFound';
-import LoginPage from './login/LoginPage';
-import RegistGoogle from './regist/RegistGoogle';
-import Parent from './chat/ChatParent';
-import Notice from './notice/Notice';
-import Tried from './tried/Tried';
-import Accompany from './accompany/Accompany';
-import Weather from './weather/Weather';
-import Webcartoon from './webcartoon/Webcartoon';
-import Admin from './admin/Admin';
-import Mypage from './mypage/Mypage';
-import MapList from './course/MapLIst';
-import MapWrite from './course/MapWrite';
-import MapDetail from './course/MapDetail';
-import KoreaNews from './koreanews/KoreaNews';
-import KoreaIssue from './koreanews/KoreaIssue';
-import NoticeDetail from './notice/NoticeDetail';
-import NoticeWrite from './notice/NoticeWrite';
-import NoticeUpdate from './notice/NoticeUpdate';
-import KoreaIssueDetail from './koreanews/KoreaIssueDetail';
-import MainPage from './main/MainPage';
-import MobileMypage from './mypage/MobileMypage';
-import MobileChatPage from './chat/MobileChatPage';
-import AccompanyDetail from './accompany/AccompanyDetail';
-import AccompanyWrite from './accompany/AccompanyWrite';
-import PriceExperiencePage from './PriceExperience/PriceExperiencePage';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Regist from "./regist/Regist";
+import NotFound from "./not_found/NotFound";
+import LoginPage from "./login/LoginPage";
+import RegistGoogle from "./regist/RegistGoogle";
+import Parent from "./chat/ChatParent";
+import Notice from "./notice/Notice";
+import Tried from "./tried/Tried";
+import Accompany from "./accompany/Accompany";
+import Weather from "./weather/Weather";
+import Webcartoon from "./webcartoon/Webcartoon";
+import Admin from "./admin/Admin";
+import Mypage from "./mypage/Mypage";
+import MapList from "./course/MapLIst";
+import MapWrite from "./course/MapWrite";
+import MapDetail from "./course/MapDetail";
+import KoreaNews from "./koreanews/KoreaNews";
+import KoreaIssue from "./koreanews/KoreaIssue";
+import NoticeDetail from "./notice/NoticeDetail";
+import NoticeWrite from "./notice/NoticeWrite";
+import NoticeUpdate from "./notice/NoticeUpdate";
+import KoreaIssueDetail from "./koreanews/KoreaIssueDetail";
+import MainPage from "./main/MainPage";
+import MobileMypage from "./mypage/MobileMypage";
+import MobileChatPage from "./chat/MobileChatPage";
+import AccompanyDetail from "./accompany/AccompanyDetail";
+import AccompanyWrite from "./accompany/AccompanyWrite";
+import PriceExperiencePage from "./PriceExperience/PriceExperiencePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/"                               element={<MainPage/>}        />
-        <Route path="/login"                          element={<LoginPage/>}       />
-        <Route path="/regist"                         element={<Regist/>}          />
-        <Route path="/regist/social/google"           element={<RegistGoogle/>}    />
-        <Route path="/notice/:noticeIdx"              element={<NoticeDetail/>}    />
-        <Route path="/notice/write"                   element={<NoticeWrite/>}     />
-        <Route path="/noticeList"                     element={<Notice/>}          />
-        <Route path="/notice/update/:noticeIdx"       element={<NoticeUpdate/>}    />
-        <Route path="/chat"                           element={<Parent/>}          />
-        <Route path="/mobilechat"                     element={<MobileChatPage/>}  />
-        <Route path="/weather"                        element={<Weather/>}         />
-        <Route path="/koreanews"                      element={<KoreaNews/>}       />
-        <Route path="/koreaissue"                     element={<KoreaIssue/>}      />
-        <Route path="/koreaissue/:index"              element={<KoreaIssueDetail/>}/>
-        <Route path="/webcartoon"                     element={<Webcartoon/>}      />
-        <Route path="/tried"                          element={<Tried/>}           />
-        <Route path="/accompany"                      element={<Accompany/>}       />
-        <Route path="/accompany/detail/:accompanyIdx" element={<AccompanyDetail/>} />
-        <Route path="/accompany/write"                element={<AccompanyWrite/>}  />
-        <Route path="/adminpage"                      element={<Admin/>}           />
-        <Route path="/mypage"                         element={<Mypage/>}          />
-        <Route path="/mobilemypage"                   element={<MobileMypage/>}    />
-        {/* <Route path="/course"                         element={<MapList />}        />
-        <Route path="/course/mapwrite"                element={<MapWrite />}       />
-        <Route path="/course/detail/:travelcourseIdx" element={<MapDetail />}      /> */}
-        <Route path="/*"                              element={<NotFound/>}        />
-        <Route path="/koreaprice"                 element={<PriceExperiencePage />} /> 
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/regist" element={<Regist />} />
+        <Route path="/regist/social/google" element={<RegistGoogle />} />
+        <Route path="/notice/:noticeIdx" element={<NoticeDetail />} />
+        <Route path="/notice/write" element={<NoticeWrite />} />
+        <Route path="/noticeList" element={<Notice />} />
+        <Route path="/notice/update/:noticeIdx" element={<NoticeUpdate />} />
+        <Route path="/chat" element={<Parent />} />
+        <Route path="/mobilechat" element={<MobileChatPage />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/koreanews" element={<KoreaNews />} />
+        <Route path="/koreaissue" element={<KoreaIssue />} />
+        <Route path="/koreaissue/:index" element={<KoreaIssueDetail />} />
+        <Route path="/webcartoon" element={<Webcartoon />} />
+        <Route path="/tried" element={<Tried />} />
+        <Route path="/accompany" element={<Accompany />} />
+        <Route
+          path="/accompany/detail/:accompanyIdx"
+          element={<AccompanyDetail />}
+        />
+        <Route path="/accompany/write" element={<AccompanyWrite />} />
+        <Route path="/adminpage" element={<Admin />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/mobilemypage" element={<MobileMypage />} />
+        <Route path="/course" element={<MapList />} />
+        <Route path="/course/mapwrite" element={<MapWrite />} />
+        <Route path="/course/detail/:travelcourseIdx" element={<MapDetail />} />
+        <Route path="/*" element={<NotFound />} />
+        <Route path="/koreaprice" element={<PriceExperiencePage />} />
       </Routes>
     </>
   );
--- src/mypage/Mypage.js
+++ src/mypage/Mypage.js
@@ -1,52 +1,129 @@
-import './Mypage.css';
-import { useNavigate } from 'react-router-dom';
+import "./Mypage.css";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-import EditNoteIcon from '@mui/icons-material/EditNote';
-import FlipCameraIosIcon from '@mui/icons-material/FlipCameraIos';
-import { useState } from 'react';
-import Frame from '../main/Frame';
-
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import FlipCameraIosIcon from "@mui/icons-material/FlipCameraIos";
+import { useEffect, useState } from "react";
+import Frame from "../main/Frame";
+import axios from "axios";
 
 const Mypage = () => {
+  let jwtToken = null;
 
+  if (sessionStorage.getItem("token") != null) {
+    jwtToken = sessionStorage.getItem("token");
+  }
 
-    return (
-        <Frame>
-            <div id="profile-wrap">
-                <div id="profile-title">
-                    My Page
-                </div>
-                <div id="profile">
-                    <img src="https://i.pinimg.com/564x/38/eb/7a/38eb7a74270f3e480224ffe26cb9d7d3.jpg"></img>
-                    <span className="modifier" ><FlipCameraIosIcon />프로필 사진 변경</span>
-                    <p>nickName</p>
-                    <span className="modifier" ><EditNoteIcon />닉네임 변경</span>
-                </div>
-                <div>
-                <div>현재 작성한 글이 없습니다.</div>
-                <div>1번 글 어쩌구 저쩌구</div>
-                <div>2번 글 어쩌구 저쩌구</div>
-                <div>3번 글 어쩌구 저쩌구</div>
-                <div>4번 글 어쩌구 저쩌구</div>
+  const header = {
+    Authorization: `Bearer ${jwtToken}`,
+  };
+
+  useEffect(() => {
+    const userId = "jksin1992@gmail.com";
+    let tempUserId = userId.replace(".", "-");
+    axios
+      .get(`http://localhost:8080/api/mypage/${tempUserId}`, {
+        headers: header,
+      })
+      .then((response) => {
+        setTravelcourse(response.data.travelcourse);
+        setAccompany(response.data.accompany);
+        setIdealreal(response.data.idealreal);
+        setQna(response.data.qna);
+        setTried(response.data.tried);
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const [travelcourse, setTravelcourse] = useState([]);
+  const [travelcourseTitle, setTravelcourseTitle] = useState([]);
+
+  const [accompany, setAccompany] = useState([]);
+  const [accompanyTitle, setAccompanyTitle] = useState([]);
+
+  const [idealreal, setIdealreal] = useState([]);
+  const [idealrealTitle, setIdealrealTitle] = useState([]);
+
+  const [qna, setQna] = useState([]);
+  const [qnaTitle, setQnaTitle] = useState([]);
+
+  const [tried, setTried] = useState([]);
+  const [triedTitle, setTriedTitle] = useState([]);
+
+  return (
+    <Frame>
+      <div id="profile-wrap">
+        <div id="profile-title">My Page</div>
+        <div id="profile">
+          <img src="https://i.pinimg.com/564x/38/eb/7a/38eb7a74270f3e480224ffe26cb9d7d3.jpg"></img>
+          <span className="modifier">
+            <FlipCameraIosIcon />
+            프로필 사진 변경
+          </span>
+          <p>nickName</p>
+          <span className="modifier">
+            <EditNoteIcon />
+            닉네임 변경
+          </span>
+        </div>
+
+        <div>
+          {travelcourse.map((t) => (
+            <div key={t.travelcourseIdx}>
+              <p>{t.travelcourseIdx}</p>
+              <p>{t.travelcourseTitle}</p>
             </div>
-            <div>
-                <div>현재 작성한 글이 없습니다.</div>
-                <div>1번 글 어쩌구 저쩌구</div>
-                <div>2번 글 어쩌구 저쩌구</div>
-                <div>3번 글 어쩌구 저쩌구</div>
-                <div>4번 글 어쩌구 저쩌구</div>
+          ))}
+        </div>
+
+        <div>
+          {accompany.map((a) => (
+            <div key={a.accompanyIdx}>
+              <p>{a.accompanyIdx}</p>
+              <p>{a.accompanyTitle}</p>
             </div>
-            <div>
-                <div>현재 작성한 글이 없습니다.</div>
-                <div>1번 글 어쩌구 저쩌구</div>
-                <div>2번 글 어쩌구 저쩌구</div>
-                <div>3번 글 어쩌구 저쩌구</div>
-                <div>4번 글 어쩌구 저쩌구</div>
+          ))}
+        </div>
+
+        {/* 빈 배열만 들어옴 */}
+        {/* <div>
+          {idealreal.map((i) => (
+            <div key={i.idealrealIdx}>
+              <p>{i.idealrealIdx}</p>
+              <p>{i.idealrealTitle}</p>
             </div>
+          ))}
+        </div>
+
+        <div>
+          {qna.map((q) => (
+            <div key={q.qnaIndex}>
+              <p>{q.qnaIdx}</p>
+              <p>{q.qnaTitle}</p>
             </div>
-            
-        </Frame>
-    )
-}
+          ))}
+        </div>
+
+        <div>
+          {tried.map((t) => (
+            <div key={t.triedIndex}>
+              <p>{t.triedIdx}</p>
+              <p>{t.triedTitle}</p>
+            </div>
+          ))}
+        </div> */}
+
+        {/* <div>
+          <div>현재 작성한 글이 없습니다.</div>
+          <div>1번 글 어쩌구 저쩌구</div>
+          <div>2번 글 어쩌구 저쩌구</div>
+          <div>3번 글 어쩌구 저쩌구</div>
+          <div>4번 글 어쩌구 저쩌구</div>
+        </div> */}
+      </div>
+    </Frame>
+  );
+};
 
 export default Mypage;
